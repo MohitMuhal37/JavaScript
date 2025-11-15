210595063b54982557edf43479901eef05b620ae
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-11-15 21:06:21 +0530

    line of codes

--- assets/scripts/calc.js
+++ assets/scripts/calc.js
@@ -3,11 +3,12 @@ let defaultResult = 0;
 currentResult = defaultResult;
 
 function addNumbers(){
+        
         currentResult = currentResult + parseInt(userInput.value);
         outputResult(currentResult, '');
 }
 
 // currentResult = addNumbers(8, 7);
 
-addBtn.addEventlistener('click', addNumbers);
+addBtn.addEventListener('click', addNumbers)
 
