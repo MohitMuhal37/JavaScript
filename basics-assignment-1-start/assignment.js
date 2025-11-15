b4c7e32eb1391623ae08574088a7bc43b86d440e
Mohit Muhal <mohitmuhal15122003@gmail.com>
2025-11-15 19:42:26 +0530

    new folder

---
+++ basics-assignment-1-start/assignment.js
@@ -0,0 +1,11 @@
+let firstVariable = 65;
+let ourResult;
+
+ourResult = firstVariable + 18;
+
+ourResult = firstVariable - 29;
+ourResult = firstVariable * 9;
+ourResult = firstVariable / 2;
+
+alert(firstVariable);
+alert(ourResult);
